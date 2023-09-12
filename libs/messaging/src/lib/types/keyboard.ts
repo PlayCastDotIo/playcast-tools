3b8da6ad79a3cbfd289d5b189c0dcd4644c2f1a3
Trey Overton <overton@heatherstone.com>
2023-09-12 15:46:53 -0400

    fix(keyboard): Add types, methods for keyboard

--- libs/messaging/src/lib/types/keyboard.ts
+++ libs/messaging/src/lib/types/keyboard.ts
@@ -1,12 +1,69 @@
-import { PlaycastButton } from "./core";
+import { PlaycastButton } from './core';
 
 // We could make explicit keys
 export type PlaycastKey = PlaycastButton & {
   keyCode: string;
   scanCode: number;
   displayName: string;
-}
+};
 
 export type PlaycastKeyboardInputFromWebGL = {
   keys: PlaycastKey[];
+};
+
+// Not currently different, but preparing for the future
+export type PlaycastKeyboardState = PlaycastKeyboardInputFromWebGL;
+
+export type PlaycastMessageKeyboardState = {
+  target: 'keyboard';
+  action: 'state';
+  message: PlaycastKeyboardState;
+};
+
+export type PlaycastMessageKeyboardDown = {
+  target: 'keyboard';
+  action: 'down';
+  message: PlaycastKey;
+};
+
+export type PlaycastMessageKeyboardUp = {
+  target: 'keyboard';
+  action: 'up';
+  message: PlaycastKey;
+};
+
+export type PlaycastKeyboardEvent =
+  | PlaycastMessageKeyboardDown
+  | PlaycastMessageKeyboardUp;
+
+export const keyboardStateToEvents = (
+  state: PlaycastKeyboardState
+): PlaycastKeyboardEvent[] => {
+  const events: PlaycastKeyboardEvent[] = [];
+  // Only generate events with a state change
+  state.keys
+    .filter((key) => key.wasPressed || key.wasReleased)
+    .forEach((key) => {
+      if (key.wasPressed) {
+        events.push({
+          target: 'keyboard',
+          action: 'down',
+          message: {
+            ...key,
+          },
+        });
+      }
+
+      if (key.wasReleased) {
+        events.push({
+          target: 'keyboard',
+          action: 'up',
+          message: {
+            ...key,
+          },
+        });
+      }
+    });
+
+  return events;
 };
