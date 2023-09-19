8d231bf9808ff8273cd4a9f667c3160202f3b69f
Jonathan Calag <jonathancalag12393@gmail.com>
2023-09-20 00:30:14 +0800

    Vitest for user.ts

---
+++ libs/messaging/src/tests/user.spec.ts
@@ -0,0 +1,15 @@
+import { expect, test } from 'vitest'
+import { PlaycastUser } from '../lib/user'
+
+const user: unknown = {
+  id: '123',
+  auth: 'abc',
+};
+
+test('user has correct form', () => {
+  expect(user).toMatchObject<PlaycastUser>({
+    id: expect.any(String),
+    auth: expect.any(String),
+  });
+});
+
