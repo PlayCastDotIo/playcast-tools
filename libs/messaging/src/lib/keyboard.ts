96180142aa31b2f9e75a601855e441634878801d
Trey Overton <overton@heatherstone.com>
2023-09-28 10:59:46 -0400

    fix(keyboard): Produce better key events with W3C codes

--- libs/messaging/src/lib/keyboard.ts
+++ libs/messaging/src/lib/keyboard.ts
@@ -2,8 +2,18 @@ import { cloneDeep } from 'lodash';
 import { PlaycastButton } from './core';
 
 export type PlaycastKeyDetail = {
-  keyCode: string;
-  scanCode: number;
+  keyCode: number;
+  location: 'standard' | 'left' | 'right' | 'numpad';
+  source: {
+    type: string;
+    code: string | number;
+  };
+  modifiers: {
+    ctrl: boolean;
+    alt: boolean;
+    shift: boolean;
+    meta: boolean;
+  };
   displayName: string;
 };
 
@@ -53,14 +63,66 @@ export const keyboardWebGlToState = (
   return cloneDeep(input) as PlaycastKeyboardState;
 };
 
+export const keyboardUnityToW3C = (unityKeyCode: number): number => {
+  // Letters A-Z, A is Unity 15, W3C 65
+  if (unityKeyCode >= 15 && unityKeyCode <= 40) return unityKeyCode + 50;
+
+  // Digits 1-9, 1 is Unity 41, W3C 49
+  if (unityKeyCode >= 41 && unityKeyCode <= 49) return unityKeyCode + 8;
+  if (unityKeyCode === 50) return 48;
+
+  // Numberpad 0-9, 0 is Unity 84, W3C 96
+  if (unityKeyCode >= 84 && unityKeyCode <= 93) return unityKeyCode + 12;
+
+  // Function keys, F1-F12, F1 is Unity 94, W3C 112
+  if (unityKeyCode >= 94 && unityKeyCode <= 105) return unityKeyCode + 18;
+
+  // Now it's just brute force
+  const mapped = keyMap[unityKeyCode] || -1;
+  return mapped;
+};
+
+export const keyboardGetModifiers = (
+  keys: PlaycastKey[]
+): PlaycastKeyDetail['modifiers'] => {
+  return {
+    ctrl:
+      keys.filter((k) => k.isPressed && [55, 56].includes(k.keyCode)).length >
+      0,
+    alt:
+      keys.filter((k) => k.isPressed && [53, 54].includes(k.keyCode)).length >
+      0,
+    shift:
+      keys.filter((k) => k.isPressed && [51, 52].includes(k.keyCode)).length >
+      0,
+    meta:
+      keys.filter((k) => k.isPressed && [57, 58].includes(k.keyCode)).length >
+      0,
+  };
+};
+
+export const keyboardGetLocation = (
+  keyCode: number
+): PlaycastKeyDetail['location'] => {
+  if ([51, 53, 55, 57].includes(keyCode)) return 'left';
+  if ([52, 54, 56, 58].includes(keyCode)) return 'right';
+  if (keyCode >= 77 && keyCode <= 93) return 'numpad';
+  return 'standard';
+};
+
 export const keyboardStateToEvents = (
   state: PlaycastKeyboardState
 ): PlaycastKeyboardEvent[] => {
   const events: PlaycastKeyboardEvent[] = [];
+
+  const modifiers = keyboardGetModifiers(state.keys);
+
   // Only generate events with a state change
   state.keys
     .filter((key) => key.wasPressed || key.wasReleased)
     .forEach((key) => {
+      const location = keyboardGetLocation(key.keyCode);
+
       if (key.wasPressed) {
         events.push({
           target: 'keyboard',
@@ -68,8 +130,13 @@ export const keyboardStateToEvents = (
           isReply: false,
 
           message: {
-            keyCode: key.keyCode,
-            scanCode: key.scanCode,
+            keyCode: keyboardUnityToW3C(key.keyCode),
+            location: location,
+            source: {
+              type: 'unity',
+              code: key.keyCode,
+            },
+            modifiers: modifiers,
             displayName: key.displayName,
           },
         });
@@ -81,8 +148,13 @@ export const keyboardStateToEvents = (
           action: 'up',
           isReply: false,
           message: {
-            keyCode: key.keyCode,
-            scanCode: key.scanCode,
+            keyCode: keyboardUnityToW3C(key.keyCode),
+            location: location,
+            source: {
+              type: 'unity',
+              code: key.keyCode,
+            },
+            modifiers: modifiers,
             displayName: key.displayName,
           },
         });
@@ -90,4 +162,53 @@ export const keyboardStateToEvents = (
     });
 
   return events;
+};
+
+export const keyMap: { [key:number]: number } = {
+  1: 32,
+  2: 13,
+  3: 9,
+  4: 192,
+  5: 222,
+  6: 186,
+  7: 188,
+  8: 190,
+  9: 191,
+  10: 220,
+  11: 219,
+  12: 221,
+  13: 189,
+  14: 187,
+  51: 16,
+  52: 16,
+  53: 18,
+  54: 18,
+  55: 17,
+  56: 17,
+  57: 91,
+  58: 92,
+  59: 93,
+  60: 27,
+  61: 37,
+  62: 39,
+  63: 38,
+  64: 40,
+  65: 8,
+  66: 34,
+  67: 33,
+  68: 36,
+  69: 35,
+  70: 45,
+  71: 46,
+  72: 20,
+  73: 144,
+  74: 44,
+  75: 145,
+  76: 19,
+  77: 13,
+  78: 111,
+  79: 106,
+  80: 107,
+  81: 109,
+  82: 110,
 };
